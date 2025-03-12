dfc4e75fed87b502ff1668b543208e01201965c1
kthod861 <95020423+kthod861@users.noreply.github.com>
2025-03-12 22:40:13 +0100

    up

--- py_tools/Make_Grid.py
+++ py_tools/Make_Grid.py
@@ -243,7 +243,7 @@ def main_grid_creator(boxart_fold, system_name , output_resolution, ntiles, padd
 ###################################################
 
 
-system_name = "sg1000"
+system_name = "psp"
 
 bato_systems = r"F:\Boxart_Project\Batocera_Systems"
 outpath = r"F:\Boxart_Project"
--- py_tools/extract_unique_images_from_folder.py
+++ py_tools/extract_unique_images_from_folder.py
@@ -75,8 +75,8 @@ def filter_names(box_files):
 
 ## V1 not really working
 
-source_folder = r"F:\libretro-thumbnails\Sega - SG-1000\Named_Boxarts"
-destination_folder = r"F:\Boxart_Project\Batocera_Systems\sg1000"
+source_folder = r"F:\libretro-thumbnails\Sony - PlayStation Portable\Named_Boxarts"
+destination_folder = r"F:\Boxart_Project\Batocera_Systems\psp"
 min_size = 300
 
 ## simple file listing
