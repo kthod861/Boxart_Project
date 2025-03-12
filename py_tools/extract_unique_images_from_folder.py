996102093d773fad0f616e92114e4fe03a367bea
kthod861 <95020423+kthod861@users.noreply.github.com>
2025-03-12 22:10:26 +0100

    up

--- py_tools/extract_unique_images_from_folder.py
+++ py_tools/extract_unique_images_from_folder.py
@@ -75,8 +75,8 @@ def filter_names(box_files):
 
 ## V1 not really working
 
-source_folder = r"F:\libretro-thumbnails\Handheld Electronic Game\Named_Boxarts"
-destination_folder = r"F:\Boxart_Project\Batocera_Systems\gameandwatch"
+source_folder = r"F:\libretro-thumbnails\Sega - Game Gear\Named_Boxarts"
+destination_folder = r"F:\Boxart_Project\Batocera_Systems\gamegear"
 min_size = 300
 
 ## simple file listing
