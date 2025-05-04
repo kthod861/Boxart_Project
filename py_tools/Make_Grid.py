7d336be75430cd34a21e264190c2a566784576ea
kthod861 <95020423+kthod861@users.noreply.github.com>
2025-05-04 21:14:02 +0200

    up

--- py_tools/Make_Grid.py
+++ py_tools/Make_Grid.py
@@ -247,7 +247,7 @@ def main_grid_creator(boxart_fold, system_name , output_resolution, ntiles, padd
 ###################################################
 
 
-system_name = "ps4"
+system_name = "mark3"
 
 bato_systems = r"F:\Boxart_Project\Batocera_Systems"
 outpath = r"F:\Boxart_Project"
