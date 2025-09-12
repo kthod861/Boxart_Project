0145c58fb84f8ed88fea3d191fdfea891958e82d
kthod861 <95020423+kthod861@users.noreply.github.com>
2025-09-12 12:48:41 +0200

    up

--- py_tools/Make_Grid.py
+++ py_tools/Make_Grid.py
@@ -110,57 +110,6 @@ def get_resolution_allimages(img_folder):
         print(lresolutions)
         print(lamount)
 
-def makegrid( img_resolution, grid_len, limage, ouputimg):
-
-    for img in limage:
-        if img.endswith("NO"):
-            limage.remove(img)
-
-    w,h = img_resolution[0]*grid_len, img_resolution[1]*grid_len
-    reuslt = Image.new('RGB', (w,h))
-  
-
-    for X in range( grid_len ):
-        for Y in range( grid_len ):
-            choice = random.choice(limage)
-            ##a test
-            ### Generate 5 unique random numbers between 1 and 10
-            ### a = random.sample(range(1, 11), 5)
-
-            im = Image.open(choice )
-            reuslt.paste(im, (X*img_resolution[0], Y*img_resolution[1]) )
-            
-
-    reuslt.save(ouputimg)
-
-def makegrid2( img_resolution, grid_len, limage, ouputimg):
-
-    for img in limage:
-        if img.endswith("NO"):
-            limage.remove(img)
-
-    w,h = img_resolution[0]*grid_len, img_resolution[1]*grid_len
-    reuslt = Image.new('RGB', (w,h))
-  
-    amountofimages = grid_len*grid_len
-    if len(limage) >= amountofimages:
-        res = random.sample( range(0, len(limage)), amountofimages)
-    else:
-        res = []
-        for i in range( amountofimages ):
-            res.append( random.choice(limage) )
-
-    i = 0
-    for X in range( grid_len ):
-        for Y in range( grid_len ):
-            choice = limage[ res[i] ]
-
-            im = Image.open(choice )
-            reuslt.paste(im, (X*img_resolution[0], Y*img_resolution[1]) )
-            i+=1
-
-    reuslt.save(ouputimg)
-
 def makegrid_fixed_outputres(thumb_res, output_res, output_path, list_images, perline_target, padding, color_scale = 1.0):
     out_w,out_h = output_res[0],output_res[1]
     thumb_w,thumb_h = thumb_res[0],thumb_res[1]
@@ -214,10 +163,6 @@ def makegrid_fixed_outputres(thumb_res, output_res, output_path, list_images, pe
     
     result.save(output_path)
 
-def decypher_system_name(system_name):
-    nospace = system_name.strip()
-    spl = nospace.split("-")
-
 def cleanup_unidentified(imgfolder):
     limage = os.listdir(imgfolder)
 
@@ -228,8 +173,7 @@ def cleanup_unidentified(imgfolder):
         except:
             os.rename(image_path, image_path+".NO")
     
-def main_grid_creator(boxart_fold, system_name , output_resolution, ntiles, padding, ouputimg, only_compat_aspectratio = False, cleanup = False, color_scale = 1.0):
-    print(system_name)
+def main_grid_creator(boxart_fold, output_resolution, ntiles, padding, ouputimg, only_compat_aspectratio = False, cleanup = False, color_scale = 1.0):
     if only_compat_aspectratio:
         resolution, limages  = get_resolution_onlyrescompatible(boxart_fold)
     else:
@@ -239,7 +183,6 @@ def main_grid_creator(boxart_fold, system_name , output_resolution, ntiles, padd
         if cleanup:
             cleanup_unidentified(boxart_fold)
 
-        #ouputimg = os.path.join( thumb_fold, "{}.jpg".format( system_name ))
         makegrid_fixed_outputres(resolution, output_resolution, ouputimg, limages, ntiles, padding, color_scale = color_scale)
 
 
@@ -247,18 +190,15 @@ def main_grid_creator(boxart_fold, system_name , output_resolution, ntiles, padd
 ###################################################
 
 
-system_name = "mark3"
 
-bato_systems = r"F:\Boxart_Project\Batocera_Systems"
-outpath = r"F:\Boxart_Project"
-boxart_fold = os.path.join( bato_systems, system_name )
-color_scale = 1.2
+boxart_fold     = r"F:\Boxart_Project\Batocera_Systems\3do" ## path where the boxart images are stored
+output_img      = r"F:\Boxart_Project\result.jpg" ## the output file
 
+color_scale         = 1.2 ## enhancement value
+only_compat_ratio   = True ## Try to use only cross compatible aspect ratios
+resolution          = (1920,1080) ## output resolution
+ncol                = 10 ## nuber of columns
+padding             = 5 ## spacing between boxarts
 
-ouputimg169 = os.path.join( outpath, "16_9", "{}.jpg".format( system_name ))
-ouputimg43 = os.path.join( outpath, "4_3", "{}.jpg".format( system_name ))
+main_grid_creator(boxart_fold, resolution, ncol, padding, output_img, only_compat_aspectratio= only_compat_ratio, color_scale = color_scale)
 
-only_compat_ratio = False
-
-main_grid_creator(boxart_fold, system_name, (1920,1080), 10, 5, ouputimg169, only_compat_aspectratio= only_compat_ratio, color_scale = color_scale)
-main_grid_creator(boxart_fold, system_name, (960,720), 10, 5, ouputimg43, only_compat_aspectratio= only_compat_ratio, color_scale = color_scale)
---
+++ py_tools/to_png.py
@@ -0,0 +1,27 @@
+from PIL import Image, ImageEnhance
+import os
+
+def list_file(img_folder):
+    lboxartfiles = os.listdir(img_folder)
+    list_files = []
+    for box_file in lboxartfiles:
+        list_files.append( os.path.join( img_folder, box_file) )
+    return list_files
+
+
+
+
+
+img_fold_src = r"C:\Users\K_thod\Desktop\New folder"
+
+lfile = list_file(img_fold_src)
+
+for f in lfile:
+    if f.endswith(".webp"):
+        impath, imname = os.path.split( f )
+        image = Image.open(f)
+
+        imgout = imname.split('.', 1)[0]
+        out = os.path.join( impath, "{}.png".format(imgout))
+
+        image.save(out, 'PNG', optimize=True, quality=80)
