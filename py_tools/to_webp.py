b16d0daa1bf69869bcc0c10496f68a4f6c27eb4f
kthod861 <95020423+kthod861@users.noreply.github.com>
2026-02-26 22:44:54 +0100

    add

---
+++ py_tools/to_webp.py
@@ -0,0 +1,40 @@
+from PIL import Image, ImageEnhance
+import os
+
+def list_file(img_folder):
+    lboxartfiles = os.listdir(img_folder)
+    list_files = []
+    for box_file in lboxartfiles:
+        list_files.append( os.path.join( img_folder, box_file) )
+    return list_files
+
+'''
+
+img_fold_src = r"F:\Boxart_Project\4_3"
+
+lfile = list_file(img_fold_src)
+
+for f in lfile:
+    if not f.endswith(".webp"):
+        impath, imname = os.path.split( f )
+        image = Image.open(f)
+
+        imgout = imname.split('.', 1)[0]
+        out = os.path.join( impath, "{}.webp".format(imgout))
+
+        image.save(out, 'webp', optimize=True, quality=80)
+'''
+
+img_fold_src = r"C:\Users\K_thod\Desktop\New folder (2)"
+
+lfile = list_file(img_fold_src)
+
+for f in lfile:
+    if not f.endswith(".webp"):
+        impath, imname = os.path.split( f )
+        image = Image.open(f)
+
+        imgout = imname.split('.', 1)[0]
+        out = os.path.join( impath, "{}.webp".format(imgout))
+
+        image.save(out, 'webp', optimize=True, quality=80)
